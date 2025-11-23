4369750e6a753971cd7b4b09d1d7d0c6294a0f2b
Ashwath <ashwathvimal@gmail.com>
2025-11-23 17:19:08 -0500

    Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -30,8 +30,8 @@ public class BasicTeleOp extends LinearOpMode {
 
 
 // Reverse one motor so that forward power moves both wheels forward
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
         ShootMotor.setDirection(DcMotor.Direction.REVERSE); // Set shooter motor direction
 
         telemetry.addData("Status", "Initialized");
