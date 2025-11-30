c11059b2cc0424781749252e6797ce90dcd26fbb
raosu <agastyasrao@gmail.com>
2025-11-30 16:31:34 -0500

    g

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -7,7 +7,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="Basic TeleOp", group="Linear Opmode")
 public class BasicTeleOp extends LinearOpMode {
-//Hello jvid
     // Declare motors
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
