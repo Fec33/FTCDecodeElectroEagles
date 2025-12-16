064bc78665ef4c3a556b93ad81d18301da6b5d0c
fec33 <ashwathvimal@gmail.com>
2025-12-15 19:20:07 -0500

    Hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousB.java
@@ -1,4 +1,51 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AutonomousB {
-}
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name="Autonomous ", group="Linear Opmode")
+public class Autonomous extends LinearOpMode {
+    // Declare motors
+    private DcMotor frontLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor backRightDrive = null;
+    private DcMotor ShootMotor = null;
+
+    // Declare Servos
+    private CRServo servoShoot1 = null;
+    private CRServo servoShoot2 = null;
+
+    @Override
+    public void runOpMode() {
+
+// Initialize hardware
+        // Make sure to match the names in your robot's configuration
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+        ShootMotor = hardwareMap.get(DcMotor.class, "shoot_motor");
+        servoShoot1 = hardwareMap.get(CRServo.class, "Servo_Shoot_1");
+        servoShoot2 = hardwareMap.get(CRServo.class, "Servo_Shoot_2");
+
+
+// Set motor directions for an Omni-wheel (Mecanum) drive
+        // You may need to change these FORWARD/REVERSE values depending on your robot's build
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        ShootMotor.setDirection(DcMotor.Direction.REVERSE); // Set shooter motor direction
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+// Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+// Run until the end of the match (driver presses STOP)
